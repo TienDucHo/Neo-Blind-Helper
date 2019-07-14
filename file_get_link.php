bcdacc1cff6b4fe544e0778ab53dfe56df95fbc6
tydaica0501 <hotienduc05012002@gmail.com>
2019-07-14 21:07:32 +0700

    Update file_get_link.php
    Thêm vào khả năng gọi hàm get_mp3() từ text_to_speech.php
    Thêm vào các hàm cần thiết để loại bỏ kí tự html đặc biệt &nbsp;

--- file_get_link.php
+++ file_get_link.php
@@ -1,15 +1,24 @@
 <?php
 include 'simple_html_dom.php';
-
+include 'text_to_speech.php';
+//file get link tu trang chu
 $lnk = $_POST["link"];
 $html = file_get_html($lnk);
+$id = 0;
 
 foreach($html->find('h1.title_news') as $title)
 {
 	$href = $title->find('a');
 	echo $href[0]->href;
 	echo '<br>';
-	echo $title -> plaintext;
+	//khúc này để bỏ cái &nbsp ra khỏi text
+	$my_str = $title -> plaintext;
+	$my_str = htmlentities($my_str);
+    $converted = strtr($my_str, array_flip(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES))); 
+	trim($converted, chr(0xC2).chr(0xA0));
+	echo $converted;
+	get_mp3($converted,'title/title'.$id);
+	$id = $id + 1; 
 	echo '<br>';
 }
 
@@ -18,7 +27,14 @@ foreach($html->find('h4.title_news') as $title)
 	$href = $title->find('a');
 	echo $href[0]->href;
 	echo '<br>';
-	echo $title -> plaintext;
+	//echo $title -> plaintext;
+	$my_str = $title -> plaintext;
+	$my_str = htmlentities($my_str);
+	$converted = strtr($my_str, array_flip(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES))); 
+	trim($converted, chr(0xC2).chr(0xA0));
+	echo $converted;
+	get_mp3($converted,'title/title'.$id);
+	$id = $id + 1;
 	echo '<br>';
 }
 
